a0f3d300c6a1b773e9a313f70d83b98065207d11
ghese <kevinkv@stud.ntnu.no>
2019-03-04 18:18:27 +0100

    DVL and Odometry topics fixed
    Both DVL and Odometry topics are now functioning and can enter test
    phase. Still need to implement the pressure topic.

--- scripts/publisherDVL1000.py
+++ scripts/publisherDVL1000.py
@@ -5,6 +5,8 @@ import time
 from websocket import create_connection
 from std_msgs.msg import String
 from dvl1000_ros.msg import DVL
+from dvl1000_ros.msg import DVLBeam
+from nav_msgs.msg import Odometry
 
 #Websocket connection stuff. Make sure the websocket-client python library is installed.
 ws = create_connection("ws://10.42.0.96:10100", subprotocols=["data-transfer-nortek"])
@@ -12,10 +14,13 @@ print("Connecting to DVL1000 via websocket...")
 result = ws.recv()
 print("Status: '%s'" % result)
 
+backupjson = ''
+unknown = "-1" #The arbitrary value set for unknown values throughout the file
+
 def cycleDVL():
 	global ws
 	retVal = ''
-	for n in range(1, 4):
+	for n in range(5):
 		result = ws.recv()
 		theData = json.loads(result)
 		if theData["id"] == 8219:
@@ -23,9 +28,14 @@ def cycleDVL():
 	return retVal
 
 def publishDVLdata():
-
+	global unknown
+	global backupjson
 	#Get JSON Data
-	theData = cycleDVL()
+	getJson = cycleDVL()
+	try:
+		theData = json.loads(getJson)
+	except:
+		theData = json.loads(backupjson)
 
 	#Alternating data each time data is recieved. Each with unique ID(4123 and 8219) and dataset. Using IF to sort this out
 	#8219 seem to have additional information like Status, Speed of Sound, Temperature
@@ -33,11 +43,14 @@ def publishDVLdata():
 	#IDs 4123 and 8219 belongs to Bottom Track mode.
 
 	pubBottom = rospy.Publisher('manta/dvl', DVL, queue_size=10)
+	pubOdo = rospy.Publisher('nav_msgs/Odometry', Odometry, queue_size=10)
 	#pubWater = rospy.Publisher('sensors/dvl/water', DVL, queue_size=10)
 	rospy.init_node('DVL1000', anonymous=False)
 	rate = rospy.Rate(8) # 8hz
 
 	theDVL = DVL()
+	theDVLBeam = DVLBeam()
+	theOdo = Odometry()
 
 	#Bottom-Trackingnumpy square
 	if theData["id"] == 8219:
@@ -71,7 +84,7 @@ def publishDVLdata():
 		BottomBeamVelMax = theData["frames"][5]["inputs"][0]["max"]
 		BottomBeamVelUnit = theData["frames"][5]["inputs"][0]["units"]
 		BottomBeamVel1Data = theData["frames"][5]["inputs"][0]["lines"][0]["data"][0]
-		BottomBeamVel2Data = theDatanumpy square["frames"][5]["inputs"][0]["lines"][1]["data"][0]
+		BottomBeamVel2Data = theData["frames"][5]["inputs"][0]["lines"][1]["data"][0]
 		BottomBeamVel3Data = theData["frames"][5]["inputs"][0]["lines"][2]["data"][0]
 		BottomBeamVel4Data = theData["frames"][5]["inputs"][0]["lines"][3]["data"][0]
 		BottomBeamVel1Valid = theData["frames"][5]["inputs"][0]["lines"][0]["valid"]
@@ -132,6 +145,7 @@ def publishDVLdata():
 		BottomXyzFom4Valid = theData["frames"][6]["inputs"][1]["lines"][3]["valid"]
 		
 		theDVL.header.stamp = rospy.Time.now()
+		theDVL.header.frame_id = "dvl_link"
 		theDVL.velocity.x = BottomXyzVel1Data
 		theDVL.velocity.y = BottomXyzVel2Data
 		if BottomXyzFom3Data > BottomXyzFom4Data:
@@ -141,16 +155,90 @@ def publishDVLdata():
 			theDVL.velocity.z = BottomXyzVel3Data
 			BottomXyzFomZbest = BottomXyzFom3Data
 			
+		#Doing covariances
+		velocity_covariance = [BottomXyzFom1Data * BottomXyzFom1Data, unknown, unknown, unknown, BottomXyzFom2Data * BottomXyzFom2Data, unknown, unknown, unknown, BottomXyzFomZbest * BottomXyzFomZbest]
+		
+		#Feeding message
+		theDVL.velocity_covariance = velocity_covariance
+		theDVL.altitude = ((BottomBeamDist1Data + BottomBeamDist2Data + BottomBeamDist3Data + BottomBeamDist4Data) / 4)
+		
+		#Individual beam data
+		beam1 = theDVLBeam
+		beam1.range = BottomBeamDist1Data
+		beam1.range_covariance = 0.0001 #TODO: find accurate value
+		beam1.velocity = BottomBeamVel1Data
+		beam1.velocity_covariance = BottomBeamFom1Data * BottomBeamFom1Data
+		beam1.pose.header.stamp = rospy.Time.now()
+		beam1.pose.pose.position.x = 0.283
+		beam1.pose.pose.position.y = 0.283
+		beam1.pose.pose.position.z = 0
+		beam1.pose.pose.orientation.x = 0.211 #Estimating values here. 25 degree tilt and 4 cm from origo
+		beam1.pose.pose.orientation.y = 0.211
+		beam1.pose.pose.orientation.z = -0.047
+		beam1.pose.pose.orientation.w = 0.953
+		
+		beam2 = theDVLBeam
+		beam2.range = BottomBeamDist2Data
+		beam2.range_covariance = 0.0001 #TODO: find accurate value
+		beam2.velocity = BottomBeamVel2Data
+		beam2.velocity_covariance = BottomBeamFom2Data * BottomBeamFom2Data
+		beam2.pose.header.stamp = rospy.Time.now()
+		beam2.pose.pose.position.x = 0.283
+		beam2.pose.pose.position.y = -0.283
+		beam2.pose.pose.position.z = 0
+		beam2.pose.pose.orientation.x = -0.211 #Estimating values here. 25 degree tilt and 4 cm from origo
+		beam2.pose.pose.orientation.y = -0.211
+		beam2.pose.pose.orientation.z = 0.047
+		beam2.pose.pose.orientation.w = -0.953
+		
+		beam3 = theDVLBeam
+		beam3.range = BottomBeamDist3Data
+		beam3.range_covariance = 0.0001 #TODO: find accurate value
+		beam3.velocity = BottomBeamVel3Data
+		beam3.velocity_covariance = BottomBeamFom3Data * BottomBeamFom3Data
+		beam3.pose.header.stamp = rospy.Time.now()
+		beam3.pose.pose.position.x = -0.283
+		beam3.pose.pose.position.y = -0.283
+		beam3.pose.pose.position.z = 0
+		beam3.pose.pose.orientation.x = -0.299 #Estimating values here. 25 degree tilt and 4 cm from origo
+		beam3.pose.pose.orientation.y = 0
+		beam3.pose.pose.orientation.z = 0.707
+		beam3.pose.pose.orientation.w = -0.641
+		
+		beam4 = theDVLBeam
+		beam4.range = BottomBeamDist4Data
+		beam4.range_covariance = 0.0001 #TODO: find accurate value
+		beam4.velocity = BottomBeamVel4Data
+		beam4.velocity_covariance = BottomBeamFom4Data * BottomBeamFom4Data
+		beam4.pose.header.stamp = rospy.Time.now()
+		beam4.pose.pose.position.x = -0.283
+		beam4.pose.pose.position.y = 0.283
+		beam4.pose.pose.position.z = 0
+		beam4.pose.pose.orientation.x = 0 #Estimating values here. 25 degree tilt and 4 cm from origo
+		beam4.pose.pose.orientation.y = 0.299
+		beam4.pose.pose.orientation.z = 0.641
+		beam4.pose.pose.orientation.w = 0.707
+		
+		theDVL.beams = [beam1, beam2, beam3, beam4]
 			
 		#Check page 49 for Z1 and Z2 and use FOM to evaluate each, 
 		#Covariance is a matrix with variance in the diagonal fields
 		#[var(x), cov(x,y) cov(x,z)|cov(x,y), var(y), cov(y,z)|cov(x,z), cov(y,z), var(z)]
-		#Concluded with using FOB² as an estimate for variance
+		#Concluded with using FOB*FOB as an estimate for variance
 		#Try to get Variance out of FOM
 		
 		#pub = rospy.Publisher('sensors/dvl/bottom', NORTEK, queue_size=10)
 		rospy.loginfo("Publishing sensor data from DVL Bottom-Track %s" % rospy.get_time())
         pubBottom.publish(theDVL)
+        backupjson = getJson
+        
+        #Odometry topic
+        theOdo.header.stamp = rospy.Time.now()
+        theOdo.header.frame_id = "dvl_link"
+        theOdo.child_frame_id = "dvl_link"
+        theOdo.twist.twist = (theDVL.velocity.x, theDVL.velocity.y, theDVL.velocity.z, unknown, unknown, unknown)
+        theOdo.twist.covariance = [BottomXyzFom1Data * BottomXyzFom1Data, unknown, unknown, unknown, unknown, unknown, unknown, BottomXyzFom2Data * BottomXyzFom2Data, unknown, unknown, unknown, unknown, unknown, unknown, BottomXyzFomZbest * BottomXyzFomZbest, unknown, unknown, unknown, unknown, unknown, unknown, unknown, unknown, unknown, unknown, unknown, unknown, unknown, unknown, unknown, unknown, unknown, unknown, unknown, unknown, unknown]
+        pubOdo.publish(theOdo)
 	
     
 	#while not rospy.is_shutdown():
--- scripts/subscriberDVL1000.py
+++ scripts/subscriberDVL1000.py
@@ -2,14 +2,11 @@
 import rospy
 from std_msgs.msg import String
 from std_msgs.msg import Float64
-from dvl1000_ros.msg import NORTEK
+from dvl1000_ros.msg import DVL
 
 def callBottom(data):
-    rospy.loginfo(rospy.get_caller_id() + "From Bottom-Tracking: %s", data.time_stamp)
-    
-def callWater(data):
-    rospy.loginfo(rospy.get_caller_id() + "From Water-Tracking %s", data.time_stamp)
-    
+    rospy.loginfo(rospy.get_caller_id() + "From Bottom-Tracking: %s", data.header.stamp)
+      
 def subscriberDVL1000():
 
 	# In ROS, nodes are uniquely named. If two nodes with the same
@@ -18,10 +15,8 @@ def subscriberDVL1000():
 	# name for our 'listener' node so that multiple listeners can
 	# run simultaneously.
 	rospy.init_node('subscriberDVL1000', anonymous=False)
-	rospy.Rate(2.5)
 
-	rospy.Subscriber("sensors/dvl/bottom", NORTEK, callBottom)
-	rospy.Subscriber("sensors/dvl/water", NORTEK, callWater)
+	rospy.Subscriber("manta/dvl", DVL, callBottom)
 
 	# spin() simply keeps python from exiting until this node is stopped
 	rospy.spin()
