ed5b2fc70e1dafc6b7bb20039a4c3e38863a9c98
Christopher Strøm <detoxte@gmail.com>
2021-07-31 19:47:39 +0200

    Remove unused parameters
    Signed-off-by: Christopher Strøm <detoxte@gmail.com>

--- scripts/publisherDVL1000.py
+++ scripts/publisherDVL1000.py
@@ -64,98 +64,36 @@ class DVL1000:
 
 		#Bottom-Trackingnumpy square
 		if dvl_data["id"] == self.data_id:
-			#Parsing Variables
-			# BottomID = dvl_data["id"]
-			# BottomMode = dvl_data["name"]
-			# BottomTime = dvl_data["timeStampStr"]
-			# BottomStatus = dvl_data["frames"][1]["inputs"][0]["lines"][0]["data"][0]
-			
-			#Speed of Sound variables
-			# BottomSpeedOfSoundMin = dvl_data["frames"][2]["inputs"][0]["min"]
-			# BottomSpeedOfSoundMax = dvl_data["frames"][2]["inputs"][0]["max"]
-			# BottomSpeedOfSoundUnit = dvl_data["frames"][2]["inputs"][0]["units"]
-			# BottomSpeedOfSoundData = dvl_data["frames"][2]["inputs"][0]["lines"][0]["data"][0]
-			
-			#Temperature varliables
-			# BottomTempMin = dvl_data["frames"][3]["inputs"][0]["min"]
-			# BottomTempMax = dvl_data["frames"][3]["inputs"][0]["max"]
-			# BottomTempUnit = dvl_data["frames"][3]["inputs"][0]["units"]
-			# BottomTempData = dvl_data["frames"][3]["inputs"][0]["lines"][0]["data"][0]
-			
-			#Pressure variables
-			# BottomPressureName = dvl_data["frames"][4]["inputs"][0]["name"]
-			# BottomPressureMin = dvl_data["frames"][4]["inputs"][0]["min"]
-			# BottomPressureMax = dvl_data["frames"][4]["inputs"][0]["max"]
+			# Pressure measurements
 			measured_pressure = dvl_data["frames"][4]["inputs"][0]["lines"][0]["data"][0]
-			# BottomPressureUnit = dvl_data["frames"][4]["inputs"][0]["units"]
 			
-			#Beam Velocity Variables
-			# BottomBeamVelMin = dvl_data["frames"][5]["inputs"][0]["min"]
-			# BottomBeamVelMax = dvl_data["frames"][5]["inputs"][0]["max"]
-			# BottomBeamVelUnit = dvl_data["frames"][5]["inputs"][0]["units"]
+			# Beam velocities and FOM (Figure Of Merit)
 			measured_velocity_beam1 = dvl_data["frames"][5]["inputs"][0]["lines"][0]["data"][0]
 			measured_velocity_beam2 = dvl_data["frames"][5]["inputs"][0]["lines"][1]["data"][0]
 			measured_velocity_beam3 = dvl_data["frames"][5]["inputs"][0]["lines"][2]["data"][0]
 			measured_velocity_beam4 = dvl_data["frames"][5]["inputs"][0]["lines"][3]["data"][0]
-			# BottomBeamVel1Valid = dvl_data["frames"][5]["inputs"][0]["lines"][0]["valid"]
-			# BottomBeamVel2Valid = dvl_data["frames"][5]["inputs"][0]["lines"][1]["valid"]
-			# BottomBeamVel3Valid = dvl_data["frames"][5]["inputs"][0]["lines"][2]["valid"]
-			# BottomBeamVel4Valid = dvl_data["frames"][5]["inputs"][0]["lines"][3]["valid"]
-			
-			#Beam FOM Variables
-			# BottomBeamFomMin = dvl_data["frames"][5]["inputs"][1]["min"]
-			# BottomBeamFomMax = dvl_data["frames"][5]["inputs"][1]["max"]
-			# BottomBeamFomUnit = dvl_data["frames"][5]["inputs"][1]["units"]
 			fom_beam1 = dvl_data["frames"][5]["inputs"][1]["lines"][0]["data"][0]
 			fom_beam2 = dvl_data["frames"][5]["inputs"][1]["lines"][1]["data"][0]
 			fom_beam3 = dvl_data["frames"][5]["inputs"][1]["lines"][2]["data"][0]
 			fom_beam4 = dvl_data["frames"][5]["inputs"][1]["lines"][3]["data"][0]
-			# BottomBeamFom1Valid = dvl_data["frames"][5]["inputs"][1]["lines"][0]["valid"]
-			# BottomBeamFom2Valid = dvl_data["frames"][5]["inputs"][1]["lines"][1]["valid"]
-			# BottomBeamFom3Valid = dvl_data["frames"][5]["inputs"][1]["lines"][2]["valid"]
-			# BottomBeamFom4Valid = dvl_data["frames"][5]["inputs"][1]["lines"][3]["valid"]
-			
-			#Beam Dist Variables
-			# BottomBeamDistMin = dvl_data["frames"][5]["inputs"][2]["min"]
-			# BottomBeamDistMax = dvl_data["frames"][5]["inputs"][2]["max"]
-			# BottomBeamDistUnit = dvl_data["frames"][5]["inputs"][2]["units"]
+
+			# Beam altitude measurements and FOM (Figure Of Merit)
 			measured_altitude_beam1 = dvl_data["frames"][5]["inputs"][2]["lines"][0]["data"][0]
 			measured_altitude_beam2 = dvl_data["frames"][5]["inputs"][2]["lines"][1]["data"][0]
 			measured_altitude_beam3 = dvl_data["frames"][5]["inputs"][2]["lines"][2]["data"][0]
 			measured_altitude_beam4 = dvl_data["frames"][5]["inputs"][2]["lines"][3]["data"][0]
-			# BottomBeamDist1Valid = dvl_data["frames"][5]["inputs"][2]["lines"][0]["valid"]
-			# BottomBeamDist2Valid = dvl_data["frames"][5]["inputs"][2]["lines"][1]["valid"]
-			# BottomBeamDist3Valid = dvl_data["frames"][5]["inputs"][2]["lines"][2]["valid"]
-			# BottomBeamDist4Valid = dvl_data["frames"][5]["inputs"][2]["lines"][3]["valid"]
-			
-			#XYZ Velocity Variables
-			# BottomXyzVelMin = dvl_data["frames"][6]["inputs"][0]["min"]
-			# BottomXyzVelMax = dvl_data["frames"][6]["inputs"][0]["max"]
-			# BottomXyzVelUnit = dvl_data["frames"][6]["inputs"][0]["units"]
+
+			# Velocity measurements and FOM in XYZ. Note the two measurements in Z
 			measured_velocity_x = dvl_data["frames"][6]["inputs"][0]["lines"][0]["data"][0]
 			measured_velocity_y = dvl_data["frames"][6]["inputs"][0]["lines"][1]["data"][0]
 			measured_velocity_z_1 = dvl_data["frames"][6]["inputs"][0]["lines"][2]["data"][0]
 			measured_velocity_z_2 = dvl_data["frames"][6]["inputs"][0]["lines"][3]["data"][0]
-			# BottomXyzVel1Valid = dvl_data["frames"][6]["inputs"][0]["lines"][0]["valid"]
-			# BottomXyzVel2Valid = dvl_data["frames"][6]["inputs"][0]["lines"][1]["valid"]
-			# BottomXyzVel3Valid = dvl_data["frames"][6]["inputs"][0]["lines"][2]["valid"]
-			# BottomXyzVel4Valid = dvl_data["frames"][6]["inputs"][0]["lines"][3]["valid"]
-			
-			#XYZ FOM Variables
-			# BottomXyzFomMin = dvl_data["frames"][6]["inputs"][1]["min"]
-			# BottomXyzFomMax = dvl_data["frames"][6]["inputs"][1]["max"]
-			# BottomXyzFomUnit = dvl_data["frames"][6]["inputs"][1]["units"]
-
-			# FOM = Figure Of Merit - indicates performance/quality of the measurements
 			fom_velocity_x = dvl_data["frames"][6]["inputs"][1]["lines"][0]["data"][0]
 			fom_velocity_y = dvl_data["frames"][6]["inputs"][1]["lines"][1]["data"][0]
 			fom_velocity_z_1 = dvl_data["frames"][6]["inputs"][1]["lines"][2]["data"][0]
 			fom_velocity_z_2 = dvl_data["frames"][6]["inputs"][1]["lines"][3]["data"][0]
-			# BottomXyzFom1Valid = dvl_data["frames"][6]["inputs"][1]["lines"][0]["valid"]
-			# BottomXyzFom2Valid = dvl_data["frames"][6]["inputs"][1]["lines"][1]["valid"]
-			# BottomXyzFom3Valid = dvl_data["frames"][6]["inputs"][1]["lines"][2]["valid"]
-			# BottomXyzFom4Valid = dvl_data["frames"][6]["inputs"][1]["lines"][3]["valid"]
 			
+			# Fill in fields in DVL twist message
 			dvl_msg.header.stamp = rospy.Time.now()
 			dvl_msg.header.frame_id = "dvl_link"
 			dvl_msg.velocity.x = measured_velocity_x
@@ -169,10 +107,7 @@ class DVL1000:
 				dvl_msg.velocity.z = measured_velocity_z_1
 				fom_velocity_z_best = fom_velocity_z_1
 				
-			#Doing covariances
 			velocity_covariance = [fom_velocity_x * fom_velocity_x, unknown, unknown, unknown, fom_velocity_y * fom_velocity_y, unknown, unknown, unknown, fom_velocity_z_best * fom_velocity_z_best]
-			
-			#Feeding message
 			dvl_msg.velocity_covariance = velocity_covariance
 			dvl_msg.altitude = ((measured_altitude_beam1 + measured_altitude_beam2 + measured_altitude_beam3 + measured_altitude_beam4) / 4)
 			
@@ -268,8 +203,6 @@ class DVL1000:
 				odometry_msg.twist.covariance = [fom_velocity_x * fom_velocity_x, unknown, unknown, unknown, unknown, unknown, unknown, fom_velocity_y * fom_velocity_y, unknown, unknown, unknown, unknown, unknown, unknown, fom_velocity_z_best * fom_velocity_z_best, unknown, unknown, unknown, unknown, unknown, unknown, unknown, unknown, unknown, unknown, unknown, unknown, unknown, unknown, unknown, unknown, unknown, unknown, unknown, unknown, unknown]
 			self.odometry_pub.publish(odometry_msg)
 
-
-			
 			#Pressure topic
 			pressure_msg.header.stamp = rospy.Time.now()
 			pressure_msg.header.frame_id = "dvl_link"
